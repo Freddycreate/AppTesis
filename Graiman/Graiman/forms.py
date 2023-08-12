aba05717c800557eb8f9685c50af35bdb059fa2f
Freddycreate <82691157+Freddycreate@users.noreply.github.com>
2023-08-12 09:20:20 -0500

    Update forms.py

--- Graiman/Graiman/forms.py
+++ Graiman/Graiman/forms.py
@@ -1,35 +1,45 @@
 from django.forms import ModelForm, TextInput
-
+from django import forms
 from molienda.models import Atomizado, Barbotina, Granulometria
+
+class DateInput(forms.DateField):
+    input_type = 'date'
+class FechaForm(forms.Form):
+    date_field = forms.DateField(widget=DateInput)
 
 
 class AtomizadoForm(ModelForm):
     class Meta:
         model = Atomizado
-        fields = '__all__'
+        fields = ['fecha', 'hora', 'codigo', 'nroSilo', 'humedad', 'observaciones', 'planta', 'barbotina', 'granulometria']
         widgets = {
+            'Fecha': TextInput(attrs={'date_field': DateInput}),
             'Planta': TextInput(attrs={'type': 'number'}),
-            'NroSilo': TextInput(attrs={'type': 'number'})
+            'NroSilo': TextInput(attrs={'type': 'number'}),
         }
 
 class BarbotinaForm(ModelForm):
     class Meta:
         model = Barbotina
-        fields = '__all__'
+        fields = ['fecha', 'hora', 'densidad', 'viscosidad', 'residuo', 'planta']
         widgets = {
             'Densidad': TextInput(attrs={'type': 'decimal'}),
-            'Viscosidad': TextInput(attrs={'type': 'number'}),
-            'Residuo': TextInput(attrs={'type': 'decimal'})
+            'Viscosidad':TextInput(attrs={'type': 'number'}),
+            'Residuo': TextInput(attrs={'type': 'decimal'}),
+            'Fecha': TextInput(attrs={'date_field': DateInput}),
+            'usuario': TextInput(attrs={'type': 'varChar'})
         }
+
 
 class GranulometriaForm(ModelForm):
     class Meta:
         model = Granulometria
-        fields = '__all__'
+        fields = ['fecha', 'hora', 'malla500', 'malla425', 'malla300', 'malla180', 'fondo', 'planta']
         widgets = {
             'Malla500': TextInput(attrs={'type': 'decimal'}),
             'Malla425': TextInput(attrs={'type': 'decimal'}),
             'Malla300': TextInput(attrs={'type': 'decimal'}),
             'Malla180': TextInput(attrs={'type': 'decimal'}),
-            'Fondo': TextInput(attrs={'type': 'decimal'})
+            'Fondo': TextInput(attrs={'type': 'decimal'}),
+            'Fecha': TextInput(attrs={'date_field': DateInput}),
         }
