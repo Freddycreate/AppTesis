83b213fb5f82052446fd22508177cc863b59d044
Freddycreate <82691157+Freddycreate@users.noreply.github.com>
2023-08-12 09:23:47 -0500

    Update models.py

--- Graiman/molienda/models.py
+++ Graiman/molienda/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f' {self.nombre} {self.apellido}'
+from django.contrib.auth.models import User
 
 
 class Planta(models.Model):
@@ -22,11 +15,12 @@ class Barbotina(models.Model):
     densidad = models.DecimalField(max_digits=10, decimal_places=2)
     viscosidad = models.IntegerField()
     residuo = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    responsable = models.CharField(max_length=100)
     planta = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f' {self.id}: {self.densidad} {self.viscosidad} {self.residuo}'
+        return f' {self.id}: {self.fecha}'
 
 
 class Granulometria(models.Model):
@@ -38,10 +32,10 @@ class Granulometria(models.Model):
     malla180 = models.CharField(max_length=50)
     fondo = models.CharField(max_length=50)
     planta = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f' {self.id}: {self.malla500} {self.malla425} {self.malla300} {self.malla180} {self.fondo}'
+        return f'{self.fecha}'
 
 
 class Atomizado(models.Model):
@@ -52,7 +46,7 @@ class Atomizado(models.Model):
     humedad = models.CharField(max_length=50)
     observaciones = models.CharField(max_length=500, null=True, blank=True)
     planta = models.ForeignKey(Planta, on_delete=models.SET_NULL, null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     granulometria = models.ForeignKey(Granulometria, on_delete=models.CASCADE)
     barbotina = models.ForeignKey(Barbotina, on_delete=models.CASCADE)
 
